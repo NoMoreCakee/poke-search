fe2ddb2598a7c70163431431cf51a024ca9dd271
NoMoreCakee <76566851+NoMoreCakee@users.noreply.github.com>
2022-12-21 18:06:57 +0100

    color palette v1.0

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   purge: ['./public/index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    colors: {},
+    colors: {
+      primary: {
+        light: '#8ECAE6',
+        DEFAULT: '#219EBC',
+        dark: '#023047',
+      },
+      secondary: {
+        DEFAULT: '#FFB703',
+        dark: '#FB8500',
+      },
+    },
     extend: {},
   },
   plugins: [],
